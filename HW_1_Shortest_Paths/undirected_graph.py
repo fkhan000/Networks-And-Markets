ec77c33c85f53c6c5b6ea1069fae450e23daf5fb
fkhan000 <fakharyarkhan843@gmail.com>
2024-09-07 20:38:38 -0400

    make shortest_path function using bfs

--- HW_1_Shortest_Paths/undirected_graph.py
+++ HW_1_Shortest_Paths/undirected_graph.py
@@ -1,5 +1,7 @@
 from typing import List
 import numpy as np
+from collections import deque
+
 
 class Node:
     def __init__(self):
@@ -21,8 +23,28 @@ class UndirectedGraph:
 def create_graph(n, p):
     
     adjency_matrix = np.random.uniform(0, 1, size = (n, n)) < p
-    
+
     return UndirectedGraph(adjency_matrix)
 
+def shortest_path(G: UndirectedGraph, i: int, j: int) -> int:
+
+     queue = deque([(i, [i])])
+     visited = set()
+
+     while queue:
+          current_node, path = queue.popleft()
+
+          if G[current_node] == G[j]:
+               return len(path)
+          
+          if current_node not in visited:
+               visited.add(current_node)
+          
+          for neighbor in G[current_node].neighbors:
+               if neighbor not in visited:
+                    queue.append((neighbor, path + [neighbor]))
+     
+     return -1
 
 
+          
